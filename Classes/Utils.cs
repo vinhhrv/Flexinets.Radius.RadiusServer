d78a8c63eeffdea61d356e88e58410990d5a7df5
verne <verner@fortelius.net>
2017-06-27 18:49:01 +0300

    Add method for parsing 3GPP-User-Location-Info attributes

--- Classes/Utils.cs
+++ Classes/Utils.cs
@@ -25,5 +25,48 @@ namespace Flexinets.Radius
             }
             return hex.ToString();
         }
+
+
+        /// <summary>
+        /// Get the mccmnc as a string from a 3GPP-User-Location-Info vendor attribute. SAI and CGI only for now
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static String GetMccMncFrom3GPPLocationInfo(Byte[] bytes)
+        {
+            String mccmnc = null;
+            var type = (LocationType)bytes[0];  // hmm...
+            if (type == LocationType.CGI || type == LocationType.ECGI || type == LocationType.RAI || type == LocationType.SAI || type == LocationType.TAI)
+            {
+                var mccDigit1 = (bytes[1] & 15).ToString();
+                var mccDigit2 = ((bytes[1] & 240) >> 4).ToString();
+                var mccDigit3 = (bytes[2] & 15).ToString();
+
+                var mncDigit1 = (bytes[3] & 15).ToString();
+                var mncDigit2 = ((bytes[3] >> 4)).ToString();
+                var mncDigit3 = (bytes[2] >> 4).ToString();
+
+                mccmnc = mccDigit1 + mccDigit2 + mccDigit3 + mncDigit1 + mncDigit2;
+                if (mncDigit3 != "15")
+                {
+                    mccmnc = mccmnc + mncDigit3;
+                }
+            }
+            return mccmnc;
+        }
+
+
+        /// <summary>
+        /// 3GPP location types
+        /// </summary>
+        public enum LocationType
+        {
+            CGI = 0,
+            SAI = 1,
+            RAI = 2, 
+            TAI = 128,
+            ECGI = 129,
+            TAIAndECGI = 130
+        }
     }
 }
--- Tests/RadiusServerTests.cs
+++ Tests/RadiusServerTests.cs
@@ -5,6 +5,7 @@ using Flexinets.Radius;
 using System.Net;
 using System.IO;
 using System.Reflection;
+using System.Collections;
 
 namespace RadiusServerTests
 {
@@ -179,5 +180,26 @@ namespace RadiusServerTests
             var calculatedMessageAuthenticator = RadiusPacket.CalculateMessageAuthenticator(requestPacket, dictionary);
             Assert.AreNotEqual(expected, calculatedMessageAuthenticator);
         }
+
+
+        /// <summary>
+        /// Test message authenticator validation fail
+        /// </summary>
+        [TestMethod]
+        public void Test3GPPLocationInfoParsing()
+        {
+            var request = "01d901becff27ef45a6bc4525aa5d65f483876951f103433363838383735303636393736011b32333230313038353030373639373640666c6578696e65747304060af7e0611a1600001fe40120001031352e3020283537393333290606000000020706000000073d06000000121a0e00001fe4003e0008000000011a17000028af01113233323031303835303037363937361a0d000028af080732333230311a09000028af0a03351a09000028af0c03301a0c000028af020605b28a3e1a27000028af052130352d314239333146373339364645464537344142464646463030384530301a0c000028af0d06303830301e0b666c6578696e6574731a0c000028af0606c230840c1a0d000028af120732333230311a0c000028af0706c23084da1a0d000028af090732333230311a09000028af1a03001a09000028af1503011a10000028af160a0132f210426d1bc01a0a000028af170480011a18000028af1412383632383238303231323838323230301a0c000028af0306000000001a0e00001fe4001800080000000405060001272602120ca8378c51ef621ac229c647a85646071a1100000009170b464c4558494e45545321053136382105313736";
+            var secret = "xyzzy5461";
+
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\dictionary";
+            var dictionary = new RadiusDictionary(path);
+
+            var requestPacket = RadiusPacket.ParseRawPacket(Utils.StringToByteArray(request), dictionary, Encoding.UTF8.GetBytes(secret));
+            var locationInfo = requestPacket.GetAttribute<Byte[]>("3GPP-User-Location-Info");
+
+            Assert.AreEqual("23201", Utils.GetMccMncFrom3GPPLocationInfo(locationInfo));
+            Assert.AreEqual("23430", Utils.GetMccMncFrom3GPPLocationInfo(Utils.StringToByteArray("0032f4030921b8e8")));
+            Assert.AreEqual("310170", Utils.GetMccMncFrom3GPPLocationInfo(Utils.StringToByteArray("001300710921b8e8")));
+        }       
     }
 }
