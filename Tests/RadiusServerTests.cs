6eb85b8c4f9110eb1f79ce469d09d0f293521998
verne <verner@fortelius.net>
2017-07-04 11:31:08 +0300

    add test for accounting mcc mnc

--- Tests/RadiusServerTests.cs
+++ Tests/RadiusServerTests.cs
@@ -183,7 +183,7 @@ namespace RadiusServerTests
 
 
         /// <summary>
-        /// Test message authenticator validation fail
+        /// Test 3GPP location info parsing from authentication packet
         /// </summary>
         [TestMethod]
         public void Test3GPPLocationInfoParsing()
@@ -204,7 +204,7 @@ namespace RadiusServerTests
 
 
         /// <summary>
-        /// Test message authenticator validation fail
+        /// Test 3GPP location info parsing with TAI+ECGI in auth packet
         /// </summary>
         [TestMethod]
         public void TestLTE3GPPLocationInfoParsing()
@@ -222,6 +222,22 @@ namespace RadiusServerTests
         }
 
 
-        // 
+        /// <summary>
+        /// Test location info in accounting packet
+        /// </summary>
+        [TestMethod]
+        public void Test3GPPLocationInfoParsingAccounting()
+        {
+            var request = "04df0248c7137b6652df0e74ff5582b51fa38415011b32393530353730333030303337303640666c6578696e6574731f0e34323336363030323632303604060af7e0612806000000011a1600001fe40120001031352e3020283537393333290606000000020706000000073d06000000121a0e00001fe4003e0008000000011a17000028af01113239353035373033303030333730361a09000028af0a03351a09000028af0c03301a0c000028af0206046756361a27000028af052130352d314239333146373339363638464537344142464646463030363430301a0c000028af0d06303830301e0b666c6578696e6574731a0c000028af06063e87a4801a0c000028af0706c23084da1a0d000028af090732333230311a09000028af1a03001a09000028af1503011a0a000028af170480211a18000028af1412383635333037303230383532383730311a0c000028af0306000000002c12433233303834444130343637353633362d06000000011a0d00001fe4000200076d326d1a0e00001fe4007b0008000000011a0d00001fe4006800076161611a1400001fe4012d000e7461672d6d326d2d30311a1100001fe400fa000b64656661756c741a0e00001fe400690008000000003212433233303834444130343637353633361a0e00001fe400920008000000000c06000005dc1a10000028af160a0132f810164f3f8b1a0d000028af120732333830313706595b43850806ac1d74181a0e00001fe4001800080000000405060000e6281a1100000009170b464c4558494e45545321053232372105313238";
+            var secret = "xyzzy5461";
+
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\dictionary";
+            var dictionary = new RadiusDictionary(path);
+
+            var ltelocationid = Utils.StringToByteArray("8232f210426d32f21000013e02");
+            var requestPacket = RadiusPacket.ParseRawPacket(Utils.StringToByteArray(request), dictionary, Encoding.UTF8.GetBytes(secret));
+            var locationInfo = requestPacket.GetAttribute<Byte[]>("3GPP-User-Location-Info");
+            Assert.AreEqual("23801", Utils.GetMccMncFrom3GPPLocationInfo(locationInfo));
+        }
     }
 }
