395d6f6e6ba4ae5bd954e2f543f950d1b600f8f3
verne <verner@fortelius.net>
2017-07-28 23:56:47 +0300

    Add semi working radius client, needs work

---
+++ Classes/RadiusClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Flexinets.Radius
+{
+    public class RadiusClient
+    {
+        private readonly UdpClient _udpClient;
+        private readonly RadiusDictionary _dictionary;
+        private readonly List<Task<IRadiusPacket>> _tasks = new List<Task<IRadiusPacket>>();
+
+        public RadiusClient(IPEndPoint localEndpoint, RadiusDictionary dictionary)
+        {
+            _udpClient = new UdpClient(localEndpoint);
+            _dictionary = dictionary;
+        }
+
+
+        public async Task<IRadiusPacket> SendPacket(IRadiusPacket packet, IPEndPoint remoteEndpoint)
+        {
+            var packetBytes = packet.GetBytes(_dictionary);
+
+            await _udpClient.SendAsync(packetBytes, packetBytes.Length, remoteEndpoint);
+            Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                _udpClient.Close();
+            });
+            var response = await _udpClient.ReceiveAsync();
+
+            return RadiusPacket.ParseRawPacket(response.Buffer, _dictionary, Encoding.UTF8.GetBytes("secret"));
+        }
+    }
+}
