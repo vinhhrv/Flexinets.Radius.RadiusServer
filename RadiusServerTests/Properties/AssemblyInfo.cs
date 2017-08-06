6e75a15f1b29315dbf70bb7641727d46f624fb89
verne <verner@fortelius.net>
2017-08-06 22:10:15 +0300

    sync versions

--- RadiusServerTests/Properties/AssemblyInfo.cs
+++ RadiusServerTests/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("6c23f981-ba94-42ca-aa16-60658c8650cc")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
